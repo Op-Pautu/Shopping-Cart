ed1c9311aa6e96df802a00be16c070343ba20279
Op Pautu <pautuop@gmail.com>
2023-03-23 22:07:59 +0530

    Add font

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -3,17 +3,20 @@ import "./TopBar.scss";
 export default function TopBar() {
   return (
     <div className="topBar">
-      <ul className="topBarItems">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
+      <div className="topBarContainer">
+        <h1 className="topBarTitle">Op's Bookstore</h1>
+        <ul className="topBarItems">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">Shop</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }

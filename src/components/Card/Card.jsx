a0a71927e51d9baaa2276591a6dc1cf41a63a5f6
Op Pautu <pautuop@gmail.com>
2023-03-25 12:35:33 +0530

    Add unique images for cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,16 +4,137 @@ export default function Card() {
   return (
     <div className="card">
       <div className="cardContainer">
-        <div className="cardImg">
-          <img src="src/images/Black & Yellow Modern Architecture Book Cover.jpg" />
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Black & Yellow Modern Architecture Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
         </div>
-        <div className="cardTitle">
-          <h2>The Art Of Epic Structures </h2>
-
-          <span>Christian A. Jones</span>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Black and Red Typography Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
         </div>
-        <div className="cardPrice">
-          <p>$19.99</p>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Black And White  Modern Alone Story Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Blue Simple Motivation Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Coral and Monaco Blue Brush Strokes Creative Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Orange and White Ink Typography Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Purple and Red Illustrated Book Creator Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Purple Illustrated Angry Robot Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Red Self Help Bold Title Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/White and Black Lines Minimalist Typography Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/White Minimalist Sci-Fi Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>The Art Of Epic Structures </h2>
+            <span>Christian A. Jones</span>
+          </div>
+          <div className="cardPrice">
+            <p>$19.99</p>
+          </div>
         </div>
       </div>
     </div>

9caaff51c594bceb89b9f1a42e527d1aa677d52c
Op Pautu <pautuop@gmail.com>
2023-03-25 14:20:17 +0530

    Finish styling for cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,6 +6,43 @@ export default function Card() {
       <div className="cardContainer">
         <div className="singleCard">
           <div className="cardImg">
+            <img src="src/images/White and Black Lines Minimalist Typography Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>Introspect </h2>
+            <span>Richard Sanchez</span>
+          </div>
+          <div className="cardPrice">
+            <p>$6.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/White Minimalist Sci-Fi Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>Am I A Robot? </h2>
+            <span>John Davies</span>
+          </div>
+          <div className="cardPrice">
+            <p>$4.99</p>
+          </div>
+        </div>
+
+        <div className="singleCard">
+          <div className="cardImg">
+            <img src="src/images/Pink and White Modern Design Book Cover.jpg" />
+          </div>
+          <div className="cardTitle">
+            <h2>Will It Ever Be Enough? </h2>
+            <span>Estelle Darcy</span>
+          </div>
+          <div className="cardPrice">
+            <p>$5.99</p>
+          </div>
+        </div>
+        <div className="singleCard">
+          <div className="cardImg">
             <img src="src/images/Black & Yellow Modern Architecture Book Cover.jpg" />
           </div>
           <div className="cardTitle">
@@ -21,11 +58,11 @@ export default function Card() {
             <img src="src/images/Black and Red Typography Book Cover.jpg" />
           </div>
           <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
+            <h2>The Tale Of The Mistaken Twin </h2>
+            <span>Katie Thompson</span>
           </div>
           <div className="cardPrice">
-            <p>$19.99</p>
+            <p>$5.99</p>
           </div>
         </div>
         <div className="singleCard">
@@ -33,11 +70,11 @@ export default function Card() {
             <img src="src/images/Black And White  Modern Alone Story Book Cover.jpg" />
           </div>
           <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
+            <h2>Alone: A True Story </h2>
+            <span>Morgan Maxwell</span>
           </div>
           <div className="cardPrice">
-            <p>$19.99</p>
+            <p>$4.99</p>
           </div>
         </div>
         <div className="singleCard">
@@ -45,11 +82,11 @@ export default function Card() {
             <img src="src/images/Blue Simple Motivation Book Cover.jpg" />
           </div>
           <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
+            <h2>You Got This </h2>
+            <span>Benjamin Shah</span>
           </div>
           <div className="cardPrice">
-            <p>$19.99</p>
+            <p>$4.55</p>
           </div>
         </div>
         <div className="singleCard">
@@ -57,11 +94,11 @@ export default function Card() {
             <img src="src/images/Coral and Monaco Blue Brush Strokes Creative Book Cover.jpg" />
           </div>
           <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
+            <h2>Path To Creative Freedom </h2>
+            <span>Takehiro Kanegi</span>
           </div>
           <div className="cardPrice">
-            <p>$19.99</p>
+            <p>$4.99</p>
           </div>
         </div>
         <div className="singleCard">
@@ -69,11 +106,11 @@ export default function Card() {
             <img src="src/images/Orange and White Ink Typography Book Cover.jpg" />
           </div>
           <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
+            <h2>Write It Right </h2>
+            <span>Annie Okuda</span>
           </div>
           <div className="cardPrice">
-            <p>$19.99</p>
+            <p>$5.99</p>
           </div>
         </div>
         <div className="singleCard">
@@ -81,11 +118,11 @@ export default function Card() {
             <img src="src/images/Purple and Red Illustrated Book Creator Cover.jpg" />
           </div>
           <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
+            <h2>Amara The Brave </h2>
+            <span>Matt Zhang</span>
           </div>
           <div className="cardPrice">
-            <p>$19.99</p>
+            <p>$3.99</p>
           </div>
         </div>
         <div className="singleCard">
@@ -93,11 +130,11 @@ export default function Card() {
             <img src="src/images/Purple Illustrated Angry Robot Book Cover.jpg" />
           </div>
           <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
+            <h2>Angry Robot </h2>
+            <span>Brigette Schwartz</span>
           </div>
           <div className="cardPrice">
-            <p>$19.99</p>
+            <p>$14.99</p>
           </div>
         </div>
         <div className="singleCard">
@@ -105,35 +142,11 @@ export default function Card() {
             <img src="src/images/Red Self Help Bold Title Book Cover.jpg" />
           </div>
           <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
+            <h2>How To Build Your Self-Esteem </h2>
+            <span>Olivia Watson</span>
           </div>
           <div className="cardPrice">
-            <p>$19.99</p>
-          </div>
-        </div>
-        <div className="singleCard">
-          <div className="cardImg">
-            <img src="src/images/White and Black Lines Minimalist Typography Book Cover.jpg" />
-          </div>
-          <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
-          </div>
-          <div className="cardPrice">
-            <p>$19.99</p>
-          </div>
-        </div>
-        <div className="singleCard">
-          <div className="cardImg">
-            <img src="src/images/White Minimalist Sci-Fi Book Cover.jpg" />
-          </div>
-          <div className="cardTitle">
-            <h2>The Art Of Epic Structures </h2>
-            <span>Christian A. Jones</span>
-          </div>
-          <div className="cardPrice">
-            <p>$19.99</p>
+            <p>$9.99</p>
           </div>
         </div>
       </div>
